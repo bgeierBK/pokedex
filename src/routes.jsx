7760fcd91b01b66ee0852d14d409acda398676e2
bmgeier <geier.benjamin@gmail.com>
2024-06-29 20:13:39 -0400

    complete region routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,12 @@ import National from './Pages/National';
 import Kanto from './Pages/Kanto';
 import Johto from './Pages/Johto';
 import Hoenn from './Pages/Hoenn';
+import Sinnoh from './Pages/Sinnoh';
+import Unova from './Pages/Unova';
+import Kalos from './Pages/Kalos';
+import Alola from './Pages/Alola';
+import Galar from './Pages/Galar';
+import Paldea from './Pages/Paldea';
 import App from './Pages/App';
 
 const routes = [
@@ -26,6 +32,36 @@ const routes = [
       {
         path: '/hoenn',
         element: <Hoenn />,
+        errorElement: <Error />,
+      },
+      {
+        path: '/sinnoh',
+        element: <Sinnoh />,
+        errorElement: <Error />,
+      },
+      {
+        path: '/unova',
+        element: <Unova />,
+        errorElement: <Error />,
+      },
+      {
+        path: '/kalos',
+        element: <Kalos />,
+        errorElement: <Error />,
+      },
+      {
+        path: '/alola',
+        element: <Alola />,
+        errorElement: <Error />,
+      },
+      {
+        path: '/galar',
+        element: <Galar />,
+        errorElement: <Error />,
+      },
+      {
+        path: '/paldea',
+        element: <Paldea />,
         errorElement: <Error />,
       },
     ],
